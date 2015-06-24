2fbab62f6c7ab24438e9254b40f0f2e16d06a18e
patrick_michelberger <pmichelberger@gmx.de>
2015-06-24 17:02:15 +0200

    initial item hashing added

--- src/webcdn.js
+++ src/webcdn.js
@@ -1,5 +1,37 @@
-function WebCDN(config) {
-	var self = this;
+'use strict';
 
-	self.load = function(content_hash, dom_id, url) {};
-};
+var sha1 = require('sha1');
+
+(function(window) {
+
+    function WebCDN(config) {
+        var self = this;
+        
+        self.initHashing = function() {
+            var items = [].slice.call(document.querySelectorAll('[data-webcdn-fallback]'));
+            items.forEach(function(item) {
+            	var hash = self.getItemHash(item);
+            	item.dataset.webcdnHash = hash;
+            });
+        };
+
+        self.getItemHash = function(item) {
+            var data = getImageData(item);
+            var hash = sha1(data);
+            return hash;
+        };
+
+        function getImageData(domElement) {
+            var canvas = document.createElement('canvas');
+            canvas.width = domElement.width;
+            canvas.height = domElement.height;
+            var context = canvas.getContext('2d');
+            context.drawImage(domElement, 0, 0, domElement.width, domElement.height);
+            var data = canvas.toDataURL("image/jpeg");
+            return data;
+        };
+
+    };
+
+    window.WebCDN = WebCDN;
+})(window);
--- tests/webcdn.test.js
+++ tests/webcdn.test.js
@@ -1,26 +1,17 @@
 var expect = chai.expect;
 
-/*
-if (!Function.prototype.bind) {
-    Function.prototype.bind = function() {
-        var fn = this,
-            args = Array.prototype.slice.call(arguments),
-            context = args.shift();
-        return function() {
-            fn.apply(context, args);
-        };
-    };
-}
-*/
-
 describe('WebCDN', function() {
-	before(function() {
-		this.webcdn = new WebCDN();
-	});
+    before(function() {
+        this.webcdn = new WebCDN();
+    });
 
-	describe('.load', function() {
-		it('should xxxx', function() {
-			expect(true).to.equal(true);
-		});
-	});
+    describe('.initHashing', function() {
+        it('should hash each marked P2P object', function() {
+            this.webcdn.initHashing();
+            var items = [].slice.call(document.querySelectorAll('[data-webcdn-fallback]'));
+            items.forEach(function(item) {
+            	expect(item.dataset.webcdnHash).not.to.be.empty;
+            });
+        });
+    });
 });
