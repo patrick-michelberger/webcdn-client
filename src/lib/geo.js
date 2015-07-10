4cdd5368e854e704c051c935f826a4339702327f
patrick_michelberger <pmichelberger@gmx.de>
2015-07-10 20:11:44 +0200

    save current location in local storage

--- src/lib/geo.js
+++ src/lib/geo.js
@@ -1,23 +1,51 @@
 module.exports = function getCurrentPosition(callback) {
-    if (navigator.geolocation) {
-        var options = {};
+    var localGeolocation = getGeolocation();
+    if (!localGeolocation) {
 
-        function geo_success(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            var position = {
-                "latitude": latitude,
-                "longitude": longitude
-            };
-            callback(null, position);
-        };
-
-        function geo_error(err) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(geo_success, geo_error);
+        } else {
+            var err = new Error("Sorry, no geo position available.");
             callback(err, null);
-        };
-        navigator.geolocation.getCurrentPosition(geo_success, geo_error, options);
+        }
     } else {
-        var err = new Error("Sorry, no geo position available.");
-        callback(err, null);
+        callback(null, localGeolocation)
     }
+
+    // Helpers
+    function saveGeolocation(position) {
+        if (window.localStorage) {
+            window.localStorage.setItem("latitude", position.latitude);
+            window.localStorage.setItem("longitude", position.longitude);
+        }
+    };
+
+    function getGeolocation() {
+        if (window.localStorage && window.localStorage.getItem("latitude") && window.localStorage.getItem("longitude")) {
+            var position = {
+                "latitude": window.localStorage.getItem("latitude"),
+                "longitude": window.localStorage.getItem("longitude")
+            };
+            return position;
+        } else {
+            return false;
+        }
+    };
+
+    // Callbacks
+    function geo_success(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var position = {
+            "latitude": latitude,
+            "longitude": longitude
+        };
+        saveGeolocation(position);
+        callback(null, position);
+    };
+
+    function geo_error(err) {
+        callback(err, null);
+    };
+
 };
