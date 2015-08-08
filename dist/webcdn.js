165c7731651a10c8e3c39ad259c78929fb6610dc
patrick_michelberger <pmichelberger@gmx.de>
2015-08-08 20:23:58 +0200

    build

--- dist/webcdn.js
+++ dist/webcdn.js
@@ -3397,6 +3397,81 @@ Peernet.prototype._handleRelayMessage = function(data) {
 };
 
 },{"./peer.js":17,"events":5,"get-browser-rtc":10,"util":9}],19:[function(require,module,exports){
+module.exports = Statistics;
+
+function Statistics(settings) {
+    settings = settings || {};
+    this._host = settings.host || "webcdn-mediator.herokuapp.com";
+    this._httpPort = settings.httpPort || "80";
+    this._wsPort = settings.wsPort ||  "1337";
+    this._uuid = settings.uuid ||  false;
+    this.init();
+};
+
+Statistics.prototype.init = function() {
+    this.initWebsocket();
+};
+
+Statistics.prototype.initWebsocket = function() {
+    var self = this,
+        url = "ws://" + self._host + ':' + self._wsPort + "?id=" + self._uuid;
+
+    if (self.socket) {
+        logger.trace("Socket exist, init fail.");
+        return;
+    }
+
+    self.socket = new WebSocket(url);
+    self.socket.onclose = function(event) {
+        console.log("WebSocket.onclose", event);
+    };
+
+    self.socket.onerror = function(event) {
+        console.log("WebSocket.onerror", event);
+    };
+
+    self.socket.onmessage = function(event) {
+        var msg = JSON.parse(event.data);
+    };
+
+    self.socket.onopen = function(event) {
+        console.log("Websocket connection open to : ", self._host);
+        self.addHost();
+    };
+};
+
+Statistics.prototype.addHost = function() {
+    var data = {
+        "uuid": this._uuid,
+        "active": true
+    };
+    if (window.performance && window.performance.timing) {
+        data.performance = {};
+        data.performance.timing = window.performance.timing.toJSON();
+    }
+    this.sendMessage("host:add", data);
+};
+
+Statistics.prototype.removeHost = function() {
+    var data = {
+        "uuid": this._uuid
+    };
+    this.sendMessage("host:remove", data);
+};
+
+Statistics.prototype.sendMessage = function(type, data) {
+    var message = {
+        type: type,
+        data: data
+    };
+    this.socket.send(JSON.stringify(message));
+};
+
+Statistics.prototype.getHostStatus = function() {};
+Statistics.prototype.startPeer = function() {};
+Statistics.prototype.abortPeer = function() {};
+
+},{}],20:[function(require,module,exports){
 // Generate UUID
 var UUID = (function() {
     function b(
@@ -3424,13 +3499,14 @@ var UUID = (function() {
 })();
 
 module.exports = UUID;
-},{}],20:[function(require,module,exports){
+},{}],21:[function(require,module,exports){
 'use strict';
 var sha1 = require('sha1');
 var UUID = require('./lib/uuid.js');
 var Messenger = require('./lib/messenger.js');
 var Peernet = require('./lib/peernet.js');
 var Logger = require('./lib/logger.js');
+var Statistics = require('./lib/statistics.js');
 var getCurrentPosition = require('./lib/geo.js');
 var EventEmitter = require('events').EventEmitter;
 var inherits = require('util').inherits;
@@ -3472,6 +3548,12 @@ var inherits = require('util').inherits;
         if (!id) {
             coordinatorUrl += "?id=" + self.uuid;
         }
+
+        this.statistics = new Statistics({
+            host: "localhost",
+            wsPort: "1337",
+            uuid: this.uuid
+        });
 
         if (this._trackGeolocation) {
             this.emit('geolocation:start');
@@ -3614,4 +3696,4 @@ var inherits = require('util').inherits;
     window.WebCDN = WebCDN;
 })(window);
 
-},{"./lib/geo.js":14,"./lib/logger.js":15,"./lib/messenger.js":16,"./lib/peernet.js":18,"./lib/uuid.js":19,"events":5,"sha1":13,"util":9}]},{},[20]);
+},{"./lib/geo.js":14,"./lib/logger.js":15,"./lib/messenger.js":16,"./lib/peernet.js":18,"./lib/statistics.js":19,"./lib/uuid.js":20,"events":5,"sha1":13,"util":9}]},{},[21]);
