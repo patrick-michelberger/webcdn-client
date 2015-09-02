a7a3d5265da77abe0587d14d14aab67151a55283
patrick_michelberger <pmichelberger@gmx.de>
2015-09-02 17:45:03 +0200

    data integrity fallback added

---
+++ src/lib/utils.js
@@ -0,0 +1,39 @@
+var Utils = {};
+
+Utils.marshalBuffer = function(buffer) {
+    var str = "";
+    var bytes = new Uint8Array(buffer);
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        str += String.fromCharCode(bytes[i]);
+    }
+    return str;
+};
+
+Utils.unmarshalBuffer = function(str) {
+    var len = str.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+        bytes[i] = str.charCodeAt(i);
+    }
+    return bytes.buffer;
+};
+
+Utils.marshal = function(message) {
+    if (message.data instanceof ArrayBuffer) {
+        message.data = this.marshalBuffer(message.data);
+    }
+    return JSON.stringify(message);
+};
+
+Utils.unmarshal = function(data) {
+    var message = JSON.parse(data);
+    if (message.hasOwnProperty('data')) {
+        if (message.data !== "\n") {
+            message.data = this.unmarshalBuffer(message.data);
+        }
+    }
+    return message;
+};
+
+module.exports = Utils;
